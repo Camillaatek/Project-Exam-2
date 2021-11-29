2202145143d3088ec3abd949eb2d7b030360bc97
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2021-11-29 20:53:05 +0100

    mixins

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,28 @@
 const url = 'http://localhost:1337/Products';
 
-const resultsDoc = document.querySelector('.featuredProducts')
-// if (products.featured === true) {
+const resultsDoc = document.querySelector('.results')
+
     const getProducts = async () => {
     const response = await fetch(url);
-    
+
     const products = await response.json();
     console.log(products)
 
     resultsDoc.innerHTML ='';
 
     for (let i = 0; i <products.length; i++) {
-        if (i === 10) {
+        if (i === 4) {
             break;
         }
 
-        resultsDoc.innerHTML += `<div class="featuredProducts">
+        resultsDoc.innerHTML += `<div class="result">
+        <img src="${products[i].image_url}">
         <h2>${products[i].title}</h2>
-        <img src="${products[i].image}">
         <p>${products[i].description}</p>
         <h3>${products[i].price}</h3>
+        <button>ADD TO CART</button>
         </div>`;
     };
 }
-// }
 
 getProducts();
