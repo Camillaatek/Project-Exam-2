ccea03cb18894a077ea48e2888ad0dd66f1a765f
Camillaatek <69843303+Camillaatek@users.noreply.github.com>
2021-12-18 08:49:57 +0100

    fixed active navbar

--- JS/Components/Common/createMenu.js
+++ JS/Components/Common/createMenu.js
@@ -42,4 +42,18 @@ export default function createMenu(){
   </div>
 </nav>`;
 logoutButton();
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
 }
+
